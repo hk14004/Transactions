ff5cd12c0a7fb55e9ca0c7c0901c2921b75ce87f
hk14004 <hardijs.kirsis@gmail.com>
2023-05-05 16:45:12 +0300

    Initial Commit

---
+++ Transactions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Transactions
+//
+//  Created by Hardijs Ķirsis on 05/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
